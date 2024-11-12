c1e80e00ce03ee0fd1b039fc3f6c984309bd477d
“ahmetercek” <ercek.ahmett@gmail.com>
2024-11-11 20:05:34 -0500

    TASK: Initialize RecipeFetch project

---
+++ RecipeFetchProject/RecipeFetchProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RecipeFetchProject
+//
+//  Created by Ahmet on 11.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
